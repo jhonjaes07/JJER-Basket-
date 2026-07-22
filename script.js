98377c6c1d8276d7bdbdfc9863de8c341e43bffa
jhonjaes07 <jhonjaes07@gmail.com>
2026-07-22 10:07:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Página de Curso de Baloncesto cargada correctamente 🏀");
+
+function compartirPagina() {
+
+    if (navigator.share) {
+
+        navigator.share({
+
+            title: "JJER Basket 🏀",
+
+            text: "🏀 Curso Gratis de Baloncesto y Acondicionamiento Físico en Residencial Los Olivos. ¡Ven a aprender, practicar y mejorar!",
+
+            url: "https://jhonjaes07.github.io/JJER-Basket-/"
+
+        });
+
+    } else {
+
+        alert(
+            "Copia y comparte este enlace:\n\n" +
+            "https://jhonjaes07.github.io/JJER-Basket-/"
+        );
+
+    }
+
+}
